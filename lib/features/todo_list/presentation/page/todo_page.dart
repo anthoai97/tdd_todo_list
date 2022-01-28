3c5d88f6855f104e5548b88454de6f440bbb3263
An Thoại <anquach.dev@gmail.com>
2022-01-28 18:10:49 +0700

    update list Ui

---
+++ lib/features/todo_list/presentation/page/todo_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ttd_todo_list/di/injection_container.dart';
+import 'package:ttd_todo_list/features/todo_list/presentation/bloc/todo_bloc.dart';
+import 'package:ttd_todo_list/themes/styles.dart';
+import 'package:flutter/foundation.dart';
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.withOpacity(0.3),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Tasks',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: AppColors.primary,
+      ),
+      body: _buildBody(context),
+    );
+  }
+
+  _buildBody(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
+    return BlocProvider(
+      create: (_) => sl<TodoBloc>(),
+      child: Container(
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 15).copyWith(top: 15),
+          itemCount: 20,
+          itemBuilder: (_, index) => Container(
+            margin: const EdgeInsets.only(bottom: 10),
+            padding:
+                const EdgeInsets.only(top: 10, bottom: 10, left: 15, right: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.4),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: const Offset(-1, 1), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: const Expanded(
+                    child: Text(
+                      'coans dasd ad asd asd asd asd asd asd asd sadas das dasd as',
+                      style: TextStyle(fontSize: 14, height: 1.3),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: Checkbox(
+                    checkColor: Colors.white,
+                    fillColor: MaterialStateProperty.resolveWith(getColor),
+                    value: false,
+                    onChanged: (bool? value) {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/main_tabbed_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:ttd_todo_list/features/todo_list/presentation/page/todo_page.dart';
+import 'package:ttd_todo_list/themes/styles.dart';
+
+class MainTabbedScreen extends StatefulWidget {
+  const MainTabbedScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainTabbedScreenState createState() => _MainTabbedScreenState();
+}
+
+class _MainTabbedScreenState extends State<MainTabbedScreen>
+    with AutomaticKeepAliveClientMixin {
+  final PageController _pageController = PageController(
+    initialPage: 0,
+    keepPage: true,
+  );
+
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var items = [
+      BottomNavigationBarItem(
+        label: "All",
+        icon: const Icon(Icons.list),
+        activeIcon: Icon(
+          Icons.list,
+          color: AppColors.primary,
+        ),
+      ),
+      BottomNavigationBarItem(
+        label: "Complete",
+        icon: const Icon(Icons.check),
+        activeIcon: Icon(
+          Icons.check,
+          color: AppColors.primary,
+        ),
+      ),
+      BottomNavigationBarItem(
+        label: "Incomplete",
+        icon: const Icon(Icons.clear),
+        activeIcon: Icon(
+          Icons.clear,
+          color: AppColors.primary,
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: items,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          _pageController.jumpToPage(index);
+        },
+        backgroundColor: Colors.white,
+        elevation: 2,
+        selectedLabelStyle: TextStyle(color: AppColors.primary, fontSize: 9),
+        unselectedLabelStyle: TextStyle(color: Colors.black, fontSize: 8),
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        mouseCursor: SystemMouseCursors.none,
+      ),
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: items.map((BottomNavigationBarItem e) {
+          int index = items.indexOf(e);
+          return _bodyForTab(context, index);
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _bodyForTab(BuildContext context, int index) {
+    var body;
+    switch (index) {
+      case 0:
+        body = const TodoPage(
+          title: 'Todo App',
+        );
+        break;
+      case 1:
+        body = Container();
+        break;
+      case 2:
+        body = Container();
+        break;
+      default:
+        body = Container();
+    }
+    return body;
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
